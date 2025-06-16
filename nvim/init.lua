2d7de24c4d2f0b23b0bae4fac3dd1f4dc612f171
Kishan <152014111+Kishandharan@users.noreply.github.com>
2025-06-16 15:38:38 +0530

    Installed Nvim Notify and Noice.Nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -32,6 +32,8 @@ require("lazy").setup({
     lazy = false,
     priority = 1000,
   },
+  { 'rcarriga/nvim-notify' },
+  { 'folke/noice.nvim' },
 })
 
 -- Configuring plugins, tweak these as you want
@@ -85,7 +87,6 @@ cmp.setup({
   },
 })
 
-
 require('tokyonight').setup({
   style = "night",
   transparent = false,
@@ -122,6 +123,41 @@ require('nvim-treesitter.configs').setup {
 }
 
 vim.opt.foldenable = false
+
+vim.notify = require("notify")
+
+require("noice").setup({
+  notify = { enabled = true },
+  lsp = {
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true,
+    },
+  },
+  presets = {
+    bottom_search = false,
+    command_palette = true,
+    long_message_to_split = true,
+    inc_rename = false,
+    lsp_doc_border = false,
+  },
+  views = {
+    cmdline_popup = {
+      position = { row = 5, col = "50%" },
+      size = { width = 60, height = "auto" },
+      border = { style = "rounded", padding = { 1, 2 } },
+      win_options = { winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" } },
+    },
+    popupmenu = {
+      relative = "editor",
+      position = { row = 8, col = "50%" },
+      size = { width = 60, height = 10 },
+      border = { style = "rounded", padding = { 0, 1 } },
+      win_options = { winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" } },
+    },
+  },
+})
 
 -- Configuring Keymaps, tweak these as you want
 local opts = {noremap = true, silent = true}
